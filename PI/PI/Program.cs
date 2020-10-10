b8e2c47abd9371ff6b94437944cbcb5682cc33f9
frozen3bits <47491170+frozen3bits@users.noreply.github.com>
2020-10-10 20:46:49 +0800

    Add files via upload

---
+++ PI/PI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal x = 1;
+            long count = 0L;
+            decimal pi = 0;
+            DateTime now1 = DateTime.Now;
+            int time1 = Convert.ToInt32(now1.ToString("ss"));
+            while (1/x >0.00000001M)
+            {
+                pi += (1 / x);
+                x+=2;
+                pi -= (1 / x);
+                x+=2;
+                count += 2;
+            }
+            pi *= 4;
+            DateTime now2 = DateTime.Now;
+            int time2 = Convert.ToInt32(now2.ToString("ss"));
+            int time = time2 - time1;
+            Console.WriteLine("π值"+pi);
+            Console.WriteLine("次数为" + count);
+            Console.WriteLine("时间为" + time+ "秒");
+        }
+    }
+}
